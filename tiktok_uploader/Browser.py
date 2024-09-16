a0883f0e58b263727f0889c2bfa60f17dd3f874e
Giant09dev <aidimi2810.sgn@gmail.com>
2024-09-17 02:20:57 +0700

    use selenium stealth

--- tiktok_uploader/Browser.py
+++ tiktok_uploader/Browser.py
@@ -2,20 +2,21 @@ from .cookies import load_cookies_from_file, save_cookies_to_file
 from fake_useragent import UserAgent, FakeUserAgentError
 import undetected_chromedriver as uc
 import threading, os
-
+from selenium_stealth import stealth  # Importing selenium-stealth
 
 WITH_PROXIES = False
+CHROME_BINARY_PATH = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+CHROME_DRIVER_PATH = r"C:\Program Files\WebDriver\chromedriver-win64\chromedriver.exe"
 
 class Browser:
+    
     __instance = None
 
     @staticmethod
     def get():
-        # print("Browser.getBrowser() called")
         if Browser.__instance is None:
             with threading.Lock():
                 if Browser.__instance is None:
-                    # print("Creating new browser instance due to no instance found")
                     Browser.__instance = Browser()
         return Browser.__instance
 
@@ -26,20 +27,30 @@ class Browser:
             Browser.__instance = self
         self.user_agent = ""
         options = uc.ChromeOptions()
+        options.binary_location = CHROME_BINARY_PATH
+
         # Proxies not supported on login.
         # if WITH_PROXIES:
         #     options.add_argument('--proxy-server={}'.format(PROXIES[0]))
+
         self._driver = uc.Chrome(options=options)
+        from selenium_stealth import stealth
+
+        # Apply stealth settings after initializing the driver
+        stealth(self._driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            fix_hairline=True,
+            use_stealth=True)
         self.with_random_user_agent()
 
     def with_random_user_agent(self, fallback=None):
-        """Set random user agent.
-        NOTE: This could fail with `FakeUserAgentError`.
-        Provide `fallback` str to set the user agent to the provided string, in case it fails. 
-        If fallback is not provided the exception is re-raised"""
-
+        """Set random user agent."""
         try:
             self.user_agent = UserAgent().random
+            print(f"Using User Agent: {self.user_agent}")  # Debugging line
         except FakeUserAgentError as e:
             if fallback:
                 self.user_agent = fallback
@@ -62,5 +73,4 @@ class Browser:
 
 if __name__ == "__main__":
     import os
-    # get current relative path of this file.
     print(os.path.dirname(os.path.abspath(__file__)))
